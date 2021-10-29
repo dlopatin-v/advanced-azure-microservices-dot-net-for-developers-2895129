0925e2e7ab68c4daaea141125527ab46c7f5546b
Denis Lopatin <stalk.sdv@gmail.com>
2021-10-29 15:17:44 +0200

    Consuming the integration event from Azure Service Bus

---
+++ src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlaggedForAdoptionIntegrationEventHandler.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WisdomPetMedicine.Pet.Api.IntegrationEvents;
+
+namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents
+{
+    public class PetFlaggedForAdoptionIntegrationEventHandler: BackgroundService
+    {
+        private readonly ILogger<PetFlaggedForAdoptionIntegrationEventHandler> _logger;
+        private readonly ServiceBusClient _client;
+        private readonly ServiceBusProcessor _processor;
+
+        public PetFlaggedForAdoptionIntegrationEventHandler(ILogger<PetFlaggedForAdoptionIntegrationEventHandler> logger, IConfiguration configuration)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _client = new ServiceBusClient(configuration["ServiceBus:ConnectionString"]);
+            _processor = _client.CreateProcessor(configuration["ServiceBus:TopicName"], configuration["ServiceBus:SubscriptionName"]);
+            _processor.ProcessMessageAsync += Processor_ProcessMessageAsync;
+            _processor.ProcessErrorAsync += Processor_ProcessErrorAsync;
+        }
+
+        private Task Processor_ProcessErrorAsync(ProcessErrorEventArgs args)
+        {
+            _logger.LogInformation(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task Processor_ProcessMessageAsync(ProcessMessageEventArgs args)
+        {
+            var body = args.Message.Body.ToString();
+            var theEvent = JsonConvert.DeserializeObject<PetFlaggedForAdoptionIntegrationEvent>(body);
+            await args.CompleteMessageAsync(args.Message);
+            _logger.LogInformation(body);
+
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await _processor.StartProcessingAsync(stoppingToken);
+        }
+
+        public async override Task StopAsync(CancellationToken stoppingToken)
+        {
+            await _processor.StopProcessingAsync(stoppingToken);
+        }
+    }
+}
